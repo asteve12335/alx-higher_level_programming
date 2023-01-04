0221bc9f82b2b7c99fc3d55d820ea10d6aa62671
asteve12335 <asteve12335@gmail.com>
2023-01-04 00:04:29 +0000

    FizzBuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,6 +2,8 @@
 def fizzbuzz():
     for i in range(101):
         if i != 100:
+            if i == 0:
+                print("{} ".format(i), end="")
             if i % 15 == 0:
                 print("{} ".format("FizzBuzz"), end="")
             elif i % 5 == 0:

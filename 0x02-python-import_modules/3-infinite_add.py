f266e0cb58f724b8327c3af2281ceffaaf4e495e
asteve12335 <asteve12335@gmail.com>
2023-01-05 13:55:48 +0000

    Sum of int args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,10 +4,6 @@ if __name__ == "__main__":
 
     sum_args = 0
     n = len(argv)
-
-    if n == 1:
-        sum_args = 0
-    else:
-        for index in range(1, n):
-            sum_args += int(index)
+    for index in range(1, n):
+        sum_args += int(index)
     print(sum_args)

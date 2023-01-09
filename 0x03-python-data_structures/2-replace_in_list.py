25f8e18dad159fe1c5a7690c33ffae5cd3ed7af2
asteve12335 <asteve12335@gmail.com>
2023-01-09 02:26:58 +0000

    Replaces element at idx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,4 +3,5 @@ def replace_in_list(my_list, idx, element):
     if idx not in range(len(my_list)):
         return my_list
     else:
-        return (my_list[idx]=element)
+        my_list[idx] = element
+        return my_list

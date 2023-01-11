80ce86269944dd5ac4ed7e0c420acdf056bf9b04
asteve12335 <asteve12335@gmail.com>
2023-01-11 22:52:16 +0000

    Returns key with the biggest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    
+
     max_student = list(a_dictionary)[0]
     max_score = a_dictionary[max_student]
     for k, v in a_dictionary.items():

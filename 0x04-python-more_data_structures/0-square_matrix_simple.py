3b223cd72063dcb1c6f7264c726bd43d935d510e
@steve <93563603+asteve12335@users.noreply.github.com>
2023-01-11 18:07:30 +0100

    Computes square value of matrix values

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+def square_matrix_simple(matrix=[]):
+    if isinstance(matrix, list):
+        square_func = lambda x: x ** 2
+        new_matrix = []
+        n = len(matrix)
+        for i in range(n):
+            sq = list(map(square_func, matrix[i]))
+            new_matrix.append(sq)
+        return new_matrix

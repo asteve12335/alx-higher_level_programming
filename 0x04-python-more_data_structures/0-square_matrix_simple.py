fc827945e5f4837c8d2b11ba1c0141aa8a26361f
@steve <93563603+asteve12335@users.noreply.github.com>
2023-01-11 18:16:07 +0100

    Compute square of integers in matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if isinstance(matrix, list):
-        square_func = lambda x: x ** 2
         new_matrix = []
         n = len(matrix)
         for i in range(n):
-            sq = list(map(square_func, matrix[i]))
+            sq = list(map(lambda x: x ** 2, matrix[i]))
             new_matrix.append(sq)
         return new_matrix

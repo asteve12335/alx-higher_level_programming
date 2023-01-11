447b2da14fc864a303d5d22a28ad5c286f117731
@steve <93563603+asteve12335@users.noreply.github.com>
2023-01-11 23:50:52 +0100

    Returns key with biggest value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    
+    max_student = list(a_dictionary)[0]
+    max_score = a_dictionary[max_student]
+    for k, v in a_dictionary.items():
+        if v > max_score:
+            max_score = v
+            max_student = k
+    return max_student

ba465e11f7b0f4588878e16d4e01e0aa75275582
asteve12335 <asteve12335>
2023-01-23 21:36:53 +0000

    Prints first x elements of a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,18 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     if isinstance(my_list, list):
         n = 0
-        try:
-            for i in range(x):
-                if type(my_list[i]) == int:
-                    print("{:d}".format(my_list[i]), end="")
-                    n += 1
-                else:
-                    continue
-        except (ValueError, TypeError):
-            pass
-        except IndexError:
-            print()
-            print("list index out of range")
-        finally:
-            print()
-            return n
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                n += 1
+            except (ValueError, TypeError):
+                continue
+        print()
+        return n

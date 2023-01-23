e1ad3372d32fc2757de040a70c49e871bef0fa5b
asteve12335 <asteve12335>
2023-01-23 19:34:51 +0000

    Prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             for i in range(x):
                 print(my_list[i], end="")
                 num_elements += 1
-        except:
+        except IndexError:
             pass
         finally:
             print()

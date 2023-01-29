4f79f73fa57a6b3b5137a663072ff02788cd467e
asteve12335 <asteve12335>
2023-01-29 17:32:03 +0000

    Integer addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,18 +4,21 @@ The following function accepts only int and float
 as parameters.
 It adds only integers though.
 """
+
+
 def add_integer(a, b=98):
     """
     a: integer
     b: integer, default value 98
     """
+
+    if type(a) not in (int, float):
+        raise TypeError("a, must be an integer")
+    elif type(b) not in (int, float):
+        raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    if not isinstance(a, int):
-        raise TypeError("a, must be an integer")
-    if not isinstance(b, int):
-        raise TypeError("b must be an integer")
     sum = a + b
     return (sum)

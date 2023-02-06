49f551b8586b2fecebf1847ffdc82df089ade474
asteve12335 <asteve12335>
2023-02-06 21:27:30 +0000

    All attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,6 +6,6 @@ Get the list of methods of an obj
 
 def def lookup(obj):
     """
-    Returns list of available attributes and method
+    Returns list of available attributes and method:
     s"""
     return dir(obj)

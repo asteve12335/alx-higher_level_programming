ca08acb3bb7a82bbd2c334b6e06d9e3f62905a4d
asteve12335 <asteve12335>
2023-02-11 23:42:15 +0000

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""Defines class Rectangle"""
+from from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    super-class: Base
+    sub-class: Rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Class constructor
+        Initializations
+        Args:
+            width(int): rectangle width
+            heigh(int): rectangle height
+            x: an integer
+            y: an integer
+            id(int): set to None as default
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """width getter"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        if not isinstance(value, int):
+            raise TypeError("x must be ab integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value

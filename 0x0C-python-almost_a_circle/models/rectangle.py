2a6cedeb4d48c58db0edba34303990f76b632c2a
asteve12335 <asteve12335>
2023-02-12 01:10:53 +0000

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,17 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """
+        Overrides str method.
+        Returns to stdout:
+            '[Rectangle] (<id>) <x>/<y> - <width>/<height>'
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id,
+                self.__x,
+                self.__y,
+                self.__width,
+                self.__height
+                )

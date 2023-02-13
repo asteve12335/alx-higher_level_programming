2b1a60994a4b84030c070e0d01352f6104319116
@steve <93563603+asteve12335@users.noreply.github.com>
2023-02-13 01:05:27 +0100

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,6 +32,18 @@ class Base:
             return "{}"
         else:
             return json.dumps(list_dictionaries)
+    
+    @staticmethod
+    def from_json_string(json_string):
+        '''
+        Returns the list of the JSON string representation `json_string`
+        args:
+            json_string (str): A json string
+        '''
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -46,15 +58,3 @@ class Base:
                 for obj in list_objs:
                     lst.append(obj.to_dictionary())
             f.write(cls.to_json_string(lst))
-
-    @staticmethod
-    def from_json_string(json_string):
-        '''
-        Returns the list of the JSON string representation `json_string`
-        args:
-            json_string (str): A json string
-        '''
-        if json_string is None:
-            return []
-        else:
-            return json.loads(json_string)

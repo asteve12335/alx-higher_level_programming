95749b987fbe51e5ae39b559c28324fb623b4b63
@steve <93563603+asteve12335@users.noreply.github.com>
2023-04-12 10:34:32 +0100

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (process.argv.length > 3) {
+  const lst = process.argv.slice(2).map(Number);
+
+  array.splice(lst.indexOf(Math.max.apply(null, lst)), 1);
+  console.log(Math.max.apply(null, lst));
+} else {
+  console.log(0);
+}

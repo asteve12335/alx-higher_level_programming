612d703140d0cb0a2c9322cd7a79728ec7b48002
asteve12335 <asteve12335>
2023-04-12 09:38:17 +0000

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length > 3) {
   const lst = process.argv.slice(2).map(Number);
 
-  array.splice(lst.indexOf(Math.max.apply(null, lst)), 1);
+  lst.splice(lst.indexOf(Math.max.apply(null, lst)), 1);
   console.log(Math.max.apply(null, lst));
 } else {
   console.log(0);
